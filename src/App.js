798b9ce493fa27b06f878cc70bbbb109c7f0a7ad
Authoi234 <jawadkhan.authoi@gmail.com>
2023-11-03 23:04:10 +0630

    previous data is cleared and new is started

--- src/App.js
+++ src/App.js
@@ -7,29 +7,17 @@ function App() {
 
   useEffect(() => {
     fetch('http://localhost:5000/users')
-    .then((res) => res.json())
-    .then(data => setUsers(data))
-  }, []);
-
-  const handleAddUser = event => {
-    event.preventDefault();
-    const name = event.target.name.value;
-    const email = event.target.email.value;
-  }
+    .then(res => res.json())
+    .then(data => setUsers(data));
+  }, [])
 
   return (
     <div className="App">
-      <form onClick={handleAddUser}> 
-        <input type='text' name='name'/>
-        <br/>
-        <input type='email' name='email'/>
-        <br/>
-        <button type='submit'>Add User</button>
-      </form>
-
-      <h1>Users {users.length}</h1>
+      <h2>Users: {users.length}</h2>
       <div>
-        {users.map(user => <p key={user.id}>{user.name} | {user.email}</p>)}
+        {
+          users.map(user => <p key={user.id}>{user.name} {user.email}</p>)
+        }
       </div>
     </div>
   );
